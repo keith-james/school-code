fca3b9da64937d31f6a24b098caf0ff1f8ef7b54
ethancancode <71207378+ethancancode@users.noreply.github.com>
2021-07-13 15:58:18 +0530

    Update bubblesort.java

--- Programs/bubblesort.java
+++ Programs/bubblesort.java
@@ -2,27 +2,44 @@ import java.util.*;
 public class bubblesort {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+
+        System.out.println("Fill your array with 5 elements.");
         int nums[] = new int[5];
+
         for (int i = 0; i < nums.length; i++) {
             nums[i] = sc.nextInt();
         }
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < i; j++) {
-                if(nums[j] < nums[i]){
-                    int temp = nums[j];
-                    nums[j] = nums[i];
-                    nums[i] = temp;
 
+        asc_rev(nums); 
+        System.out.println();
+        dec_rev(nums);
+
+        sc.close();
+    }
+
+    private static void dec_rev(int array[]){
+
+        System.out.print("Soring in descending: ");
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if(array[j] < array[i]){
+                    int temp = array[j];
+                    array[j] = array[i];
+                    array[i] = temp;
                 }
             }
         }
-        for (int i = 0; i < nums.length; i++) {
-            System.out.println(nums[i]);
-
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
         }
-        reverse(nums);
+
     }
-    private static void reverse(int array[]){
+
+    private static void asc_rev(int array[]){
+
+        System.out.print("Sorting in ascending: ");
+
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < i; j++) {
                 if(array[j] > array[i]){
@@ -35,7 +52,7 @@ public class bubblesort {
 
         }
         for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+            System.out.print(array[i]+ " ");
 
         }
     }
