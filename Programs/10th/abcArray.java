61d2252c2aea6ec9501f3bb889c59c7b5a54cef3
Keith James <keithytjames@gmail.com>
2021-07-17 15:52:37 +0530

    Some refactoring, and added name input and table format to arraysubjects.java

--- Programs/10th/abcArray.java
+++ Programs/10th/abcArray.java
@@ -1,17 +1,22 @@
 import java.util.*;
-public class abcremapiec {
+public class abcArray {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int a[] = new int[2];
         int b[] = new int[2];
         int c[] = new int[4];
+
         for(int i = 0; i < a.length; i++){
             System.out.print("Enter a =");
             a[i] = sc.nextInt();
             System.out.print("Entre b = ");
             b[i] = sc.nextInt();
         }
+
+        sc.close();
         int j = 0;
+
         for(int i = 0; i < c.length; i++){
             if(i < a.length){
                 c[i] = a[i];
--- Programs/10th/arraysubjects.java
+++ Programs/10th/arraysubjects.java
@@ -1,27 +1,49 @@
-/*6. Accept the name, physics, chemistry and math marks of 25 students. Then display a list of the given data with Total and Average.
-bugg no name input so sad :(*/
+//6. Accept the name, physics, chemistry and math marks of 25 students. Then display a list of the given data with Total and Average.
 
 import java.util.*;
 public class arraysubjects {
     public static void main(String[] args) {
+
+        /*Initialising variables*/
         Scanner sc=new Scanner(System.in);
-        int physics[] = new int[5];
-        int maths[] = new int[5];
-        int chem[] = new int[5];
+        int numberOfStudents = 2;
+        String names[] = new String[numberOfStudents];
+        int physics[] = new int[numberOfStudents];
+        int maths[] = new int[numberOfStudents];
+        int chem[] = new int[numberOfStudents];
         int physics_sum = 0, chem_sum = 0, maths_sum = 0;
-        for(int i = 0; i < physics.length; i++){
-            System.out.print("Physics value: ");
+
+        /* INPUT */
+        for(int i = 0; i < numberOfStudents; i++){
+            System.out.println("Enter your name: ");
+            names[i] = sc.next();
+
+            System.out.print("Enter your physics marks: ");
             physics[i] = sc.nextInt();
-            System.out.print("Maths value: ");
+
+            System.out.print("Enter your maths marks: ");
             maths[i] = sc.nextInt();
-            System.out.print("Chem value: ");
+
+            System.out.print("Enter your chem marks: ");
             chem[i] = sc.nextInt();
         }
-        for(int i = 0; i < physics.length; i++){
+
+        sc.close();
+
+        /*Calculating the total*/
+        for(int i = 0; i < numberOfStudents; i++){
             physics_sum = physics_sum + physics[i];
             chem_sum = chem_sum + chem[i];
             maths_sum = maths_sum + maths[i];
         }
+
+        System.out.println("NAME \t PHYSICS CHEMISTRY MATH"); //The "\t" here is just for formatting, so that it looks good.
+
+        for(int i = 0; i < numberOfStudents; i++) {
+            System.out.println(names[i] + " \t    " + physics[i] + " \t     " + chem[i] + " \t    " + maths[i]);
+        }
+
+        /*Printing out the name, total and the average. The average is calculated here itself because it's just total divided by number of items (array length)*/
         System.out.println("Physics total: " + physics_sum + "\nPhysics avg: " + (physics_sum/physics.length));
         System.out.println("Maths total: " + maths_sum + "\nMaths avg: " + (maths_sum/physics.length));
         System.out.println("Chem total: " + chem_sum + "\nChem avg: " + (chem_sum/physics.length));
