72b114e0100327e75b89aa59e771579427f2e1f4
Jay Tea <65911490+theJayTea@users.noreply.github.com>
2021-07-25 12:20:22 +0530

    easier version with 2 variables instead of 3 :)
    yep you read it right

--- Programs/9th/floyds.java
+++ Programs/9th/floyds.java
@@ -7,20 +7,19 @@
 import java.util.*;
 public class floyds
 {
-    public static void main(String args[])
-    {
-        Scanner et=new Scanner(System.in);
-        System.out.println("Enter a limit");
-        int n=et.nextInt();
-        int floyds=1;
-        for (int i=1; i<=n; i++)
-        {
-            for(int j=1; j<=i; j++){
-                System.out.print(floyds + " ");
-                floyds++;
+import java.util.*;
+public class flodsTriangle {
+    public static void main (String args[]){
+        Scanner jt = new Scanner (System.in);
+
+        System.out.println("Enter a limit:");
+        int limit=jt.nextInt();
+
+        for(int i=1;i<=limit;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
             }
-            System.out.println();
+            System.out.print("\n");
         }
-        et.close();
     }
 }
